1822e81dc0a610d245a3b2860113fdb43e5d38d7
6HyeonHee <dbrgusgml@gmail.com>
2024-10-13 02:19:42 +0900

    24.10.13_육현희_메인슬라이더 완료

--- lib/widgets/main_slider_widget.dart
+++ lib/widgets/main_slider_widget.dart
@@ -17,7 +17,12 @@ class _MainSliderWidgetState extends State<MainSliderWidget> {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      options: CarouselOptions(height: 400.0),
+      options: CarouselOptions(
+        height: 400.0,
+        viewportFraction: 1.0, // 슬라이더가 가로로 꽉 차도록 설정
+        autoPlay: true, // 슬라이더가 자동으로 넘어가도록 설정
+        autoPlayInterval: Duration(seconds: 3), // 자동 넘김 간격 설정
+      ),
       items: widget.thumb.map((popupBoard) {
         String thumbUrl = '${widget.baseUrl}${popupBoard.thumb}'; // 이미지 URL
         print(thumbUrl);
@@ -28,13 +33,15 @@ class _MainSliderWidgetState extends State<MainSliderWidget> {
               width: MediaQuery.of(context).size.width,
               margin: EdgeInsets.symmetric(horizontal: 5.0),
               decoration: BoxDecoration(color: Colors.amber),
-              child: popupBoard.thumb != null && popupBoard.thumb!.isNotEmpty ? CachedNetworkImage(
-                imageUrl: thumbUrl,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.broken_image, size: 50),
-              )
-              : Icon(Icons.image_not_supported, size: 50), // 대체 이미지
+              child: popupBoard.thumb != null && popupBoard.thumb!.isNotEmpty
+                  ? CachedNetworkImage(
+                      imageUrl: thumbUrl,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          Icon(Icons.broken_image, size: 50),
+                    )
+                  : Icon(Icons.image_not_supported, size: 50), // 대체 이미지
             );
           },
         );
